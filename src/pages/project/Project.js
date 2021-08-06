9b7cdec4ba38f4af74992770c181234a2e09c82b
Naufal Akbar <kbrnugroho@gmail.com>
2021-08-06 10:56:39 +0700

    updated project title

--- src/pages/project/Project.js
+++ src/pages/project/Project.js
@@ -21,7 +21,7 @@ function Project() {
                   <div className="px-8 py-10 relative z-10 w-full border-4 border-gray-800 bg-gray-900 opacity-0 hover:opacity-100">
                     <h2 className="tracking-widest text-sm title-font font-medium text-blue-400 mb-1">PROJECT 01</h2>
                     <h1 className="title-font text-lg font-medium text-white mb-3">Music Player</h1>
-                    <p className="leading-relaxed">Photo booth fam kinfolk cold-pressed sriracha leggings jianbing microdosing tousled waistcoat.</p>
+                    <p className="leading-relaxed">Deskripsi.</p>
                     <Link to="/Project/music-player" class="text-blue-600 inline-flex items-center mt-5 hover:text-blue-400">
                       Learn More
                       <svg class="w-4 h-4 ml-2" viewBox="0 0 24 24" stroke="currentColor" stroke-width="2" fill="none" stroke-linecap="round" stroke-linejoin="round">
@@ -37,8 +37,8 @@ function Project() {
                   <img alt="gallery" className="absolute inset-0 w-full h-full object-cover object-center" src="https://dummyimage.com/601x361" />
                   <div className="px-8 py-10 relative z-10 w-full border-4 border-gray-800 bg-gray-900 opacity-0 hover:opacity-100">
                     <h2 className="tracking-widest text-sm title-font font-medium text-blue-400 mb-1">PROJECT 02</h2>
-                    <h1 className="title-font text-lg font-medium text-white mb-3">The Catalyzer</h1>
-                    <p className="leading-relaxed">Photo booth fam kinfolk cold-pressed sriracha leggings jianbing microdosing tousled waistcoat.</p>
+                    <h1 className="title-font text-lg font-medium text-white mb-3">Todo List</h1>
+                    <p className="leading-relaxed">Deskripsi.</p>
                     <Link to="/Project/" class="text-blue-600 inline-flex items-center mt-5 hover:text-blue-400">
                       Learn More
                       <svg class="w-4 h-4 ml-2" viewBox="0 0 24 24" stroke="currentColor" stroke-width="2" fill="none" stroke-linecap="round" stroke-linejoin="round">
@@ -54,8 +54,8 @@ function Project() {
                   <img alt="gallery" className="absolute inset-0 w-full h-full object-cover object-center" src="https://dummyimage.com/603x363" />
                   <div className="px-8 py-10 relative z-10 w-full border-4 border-gray-800 bg-gray-900 opacity-0 hover:opacity-100">
                     <h2 className="tracking-widest text-sm title-font font-medium text-blue-400 mb-1">PROJECT 03</h2>
-                    <h1 className="title-font text-lg font-medium text-white mb-3">The 400 Blows</h1>
-                    <p className="leading-relaxed">Photo booth fam kinfolk cold-pressed sriracha leggings jianbing microdosing tousled waistcoat.</p>
+                    <h1 className="title-font text-lg font-medium text-white mb-3">Face Recognition</h1>
+                    <p className="leading-relaxed">Deskripsi.</p>
                     <Link to="/Project/" class="text-blue-600 inline-flex items-center mt-5 hover:text-blue-400">
                       Learn More
                       <svg class="w-4 h-4 ml-2" viewBox="0 0 24 24" stroke="currentColor" stroke-width="2" fill="none" stroke-linecap="round" stroke-linejoin="round">
@@ -71,8 +71,8 @@ function Project() {
                   <img alt="gallery" className="absolute inset-0 w-full h-full object-cover object-center" src="https://dummyimage.com/602x362" />
                   <div className="px-8 py-10 relative z-10 w-full border-4 border-gray-800 bg-gray-900 opacity-0 hover:opacity-100">
                     <h2 className="tracking-widest text-sm title-font font-medium text-blue-400 mb-1">PROJECT 04</h2>
-                    <h1 className="title-font text-lg font-medium text-white mb-3">Neptune</h1>
-                    <p className="leading-relaxed">Photo booth fam kinfolk cold-pressed sriracha leggings jianbing microdosing tousled waistcoat.</p>
+                    <h1 className="title-font text-lg font-medium text-white mb-3">3D Card Animation</h1>
+                    <p className="leading-relaxed">Deskripsi.</p>
                     <Link to="/Project/" class="text-blue-600 inline-flex items-center mt-5 hover:text-blue-400">
                       Learn More
                       <svg class="w-4 h-4 ml-2" viewBox="0 0 24 24" stroke="currentColor" stroke-width="2" fill="none" stroke-linecap="round" stroke-linejoin="round">
@@ -88,8 +88,8 @@ function Project() {
                   <img alt="gallery" className="absolute inset-0 w-full h-full object-cover object-center" src="https://dummyimage.com/605x365" />
                   <div className="px-8 py-10 relative z-10 w-full border-4 border-gray-800 bg-gray-900 opacity-0 hover:opacity-100">
                     <h2 className="tracking-widest text-sm title-font font-medium text-blue-400 mb-1">PROJECT 05</h2>
-                    <h1 className="title-font text-lg font-medium text-white mb-3">Holden Caulfield</h1>
-                    <p className="leading-relaxed">Photo booth fam kinfolk cold-pressed sriracha leggings jianbing microdosing tousled waistcoat.</p>
+                    <h1 className="title-font text-lg font-medium text-white mb-3">Zoom Clone</h1>
+                    <p className="leading-relaxed">Deskripsi.</p>
                     <Link to="/Project/" class="text-blue-600 inline-flex items-center mt-5 hover:text-blue-400">
                       Learn More
                       <svg class="w-4 h-4 ml-2" viewBox="0 0 24 24" stroke="currentColor" stroke-width="2" fill="none" stroke-linecap="round" stroke-linejoin="round">
@@ -105,8 +105,8 @@ function Project() {
                   <img alt="gallery" className="absolute inset-0 w-full h-full object-cover object-center" src="https://dummyimage.com/606x366" />
                   <div className="px-8 py-10 relative z-10 w-full border-4 border-gray-800 bg-gray-900 opacity-0 hover:opacity-100">
                     <h2 className="tracking-widest text-sm title-font font-medium text-blue-400 mb-1">PROJECT 06</h2>
-                    <h1 className="title-font text-lg font-medium text-white mb-3">Alper Kamu</h1>
-                    <p className="leading-relaxed">Photo booth fam kinfolk cold-pressed sriracha leggings jianbing microdosing tousled waistcoat.</p>
+                    <h1 className="title-font text-lg font-medium text-white mb-3">RCRP Landing Page</h1>
+                    <p className="leading-relaxed">Deskripsi.</p>
                     <Link to="/Project/" class="text-blue-600 inline-flex items-center mt-5 hover:text-blue-400">
                       Learn More
                       <svg class="w-4 h-4 ml-2" viewBox="0 0 24 24" stroke="currentColor" stroke-width="2" fill="none" stroke-linecap="round" stroke-linejoin="round">
