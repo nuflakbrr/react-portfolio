4f89d5a668184be979dac7cf8c3e3da1477df009
Naufal Akbar <kbrnugroho@gmail.com>
2021-08-11 09:09:59 +0700

    fix notfound page

--- src/components/NotFound.js
+++ src/components/NotFound.js
@@ -4,14 +4,14 @@ import Seo from "../components/Seo";
 
 function NotFound() {
   return (
-    <div class="bg-blue-800 relative overflow-hidden h-screen">
+    <div class="bg-gray-900 relative overflow-hidden h-screen">
       <Seo title={"Not Found"} />
       <div class="inset-0 bg-black opacity-25 absolute"></div>
       <div class="container mx-auto px-6 md:px-12 relative z-10 flex items-center py-32 xl:py-40">
         <div class="w-full font-mono flex flex-col items-center relative z-10">
           <h1 class="font-extrabold text-5xl text-center text-white leading-tight mt-4">Oops!, This page under construction</h1>
           <p class="font-extrabold text-8xl my-44 text-white">404</p>
-          <Link to="/" className="text-white underline hover:text-gray-400">
+          <Link to="/" className="text-white underline hover:text-blue-500">
             Back to Home
           </Link>
         </div>
