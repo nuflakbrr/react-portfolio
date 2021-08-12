276485fc54fb66bd90719ada20433345b232b8d0
Naufal Akbar <kbrnugroho@gmail.com>
2021-08-12 09:43:09 +0700

    added picture face

--- src/pages/project/Project.js
+++ src/pages/project/Project.js
@@ -4,6 +4,7 @@ import Footer from "../../components/Footer";
 import Navbars from "../../components/Navbars";
 import Fade from "react-reveal/Fade";
 import Music from "../images/pro-mus.jpg";
+import Face from "../images/pro-fac.jpg";
 import { Link } from "react-router-dom";
 
 function Project() {
@@ -40,7 +41,7 @@ function Project() {
                 </div>
                 <div className="lg:w-1/3 sm:w-1/2 p-4 w-5/6">
                   <div className="flex relative">
-                    <img alt="gallery" className="absolute inset-0 w-full h-full object-cover object-center" src="https://dummyimage.com/601x361" />
+                    <img alt="gallery" className="absolute inset-0 w-full h-full object-cover object-center" src={Face} />
                     <div className="px-8 py-10 relative z-10 w-full border-4 border-gray-800 bg-gray-900 opacity-0 hover:opacity-100">
                       <h2 className="tracking-widest text-sm title-font font-medium text-blue-400 mb-1">PROJECT 02</h2>
                       <h1 className="title-font text-md font-medium text-white mb-3">Face Recognition</h1>
--- src/pages/project/projectdetails/FaceRecognition.js
+++ src/pages/project/projectdetails/FaceRecognition.js
@@ -1,8 +1,9 @@
 import React from "react";
-import { Link } from "react-router-dom";
 import Footer from "../../../components/Footer";
 import Navbars from "../../../components/Navbars";
 import Seo from "../../../components/Seo";
+import FaceBes from "../../images/pro-facBes.jpg";
+import { Link } from "react-router-dom";
 
 function FaceRecognition() {
   const RenderFace = () => {
@@ -11,8 +12,8 @@ function FaceRecognition() {
         <section className="text-gray-400 bg-gray-900 body-font">
           <div className="container px-5 py-24 mx-auto flex flex-col">
             <div className="lg:w-4/6 mx-auto">
-              <div className="rounded-lg h-64 overflow-hidden">
-                <img alt="content" className="object-cover object-center h-full w-full" src="https://dummyimage.com/1200x500" />
+              <div className="rounded-lg overflow-hidden">
+                <img alt="content" className="object-cover object-center h-full w-full" src={FaceBes} />
               </div>
               <div className="flex flex-col sm:flex-row mt-10">
                 <div className="sm:w-1/3 text-center sm:pr-8 sm:py-8">
