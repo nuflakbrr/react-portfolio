aea1fa9e5982f7851f33a8339b1424b22d08a5ab
Naufal Akbar <kbrnugroho@gmail.com>
2021-08-12 09:37:51 +0700

    added picture music

--- src/pages/project/Project.js
+++ src/pages/project/Project.js
@@ -3,6 +3,7 @@ import Seo from "../../components/Seo";
 import Footer from "../../components/Footer";
 import Navbars from "../../components/Navbars";
 import Fade from "react-reveal/Fade";
+import Music from "../images/pro-mus.jpg";
 import { Link } from "react-router-dom";
 
 function Project() {
@@ -22,7 +23,7 @@ function Project() {
               <div className="flex flex-wrap -m-4 items-center justify-center">
                 <div className="lg:w-1/3 sm:w-1/2 p-4 w-5/6">
                   <div className="flex relative">
-                    <img alt="gallery" className="absolute inset-0 w-full h-full object-cover object-center" src="https://dummyimage.com/600x360" />
+                    <img alt="gallery" className="absolute inset-0 w-full h-full object-cover object-center" src={Music} />
                     <div className="px-8 py-10 relative z-10 w-full border-4 border-gray-800 bg-gray-900 opacity-0 hover:opacity-100">
                       <h2 className="tracking-widest text-sm title-font font-medium text-blue-400 mb-1">PROJECT 01</h2>
                       <h1 className="title-font text-md font-medium text-white mb-3">Music Player</h1>
