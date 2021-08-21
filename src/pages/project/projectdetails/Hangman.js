191176c98fe9acb148307ef7fbb82ad913902cda
Naufal Akbar <kbrnugroho@gmail.com>
2021-08-21 14:51:24 +0700

    update hangman

--- src/pages/project/projectdetails/Hangman.js
+++ src/pages/project/projectdetails/Hangman.js
@@ -2,7 +2,7 @@ import React from "react";
 import Footer from "../../../components/Footer";
 import Navbars from "../../../components/Navbars";
 import Seo from "../../../components/Seo";
-import TridiBes from "../../images/pro-3dBes.jpg";
+import HangBes from "../../images/pro-hangBes.gif";
 import Profile from "../../images/profile.jpg";
 import { Link } from "react-router-dom";
 
@@ -14,7 +14,7 @@ function Hangman() {
           <div className="container px-5 py-24 mx-auto flex flex-col">
             <div className="lg:w-4/6 mx-auto">
               <div className="rounded-lg overflow-hidden">
-                <img alt="content" className="object-cover object-center h-full w-full" src={TridiBes} />
+                <img alt="content" className="object-cover object-center h-full w-full" src={HangBes} />
               </div>
               <div className="flex flex-col sm:flex-row mt-10">
                 <div className="sm:w-1/3 text-center sm:pr-8 sm:py-8">
@@ -34,17 +34,14 @@ function Hangman() {
                   </div>
                 </div>
                 <div className="sm:w-2/3 sm:pl-8 sm:py-8 sm:border-l border-gray-800 sm:border-t-0 border-t mt-4 pt-4 sm:mt-0 text-justify sm:text-justify">
-                  <p className="leading-relaxed text-lg mb-4">In this project I create an 3D animation card product website.</p>
+                  <p className="leading-relaxed text-lg mb-4">In this project I want to create a hangman games to learn about the standard Indonesian language that we just heard. I create this with React.js</p>
                   <p className="leading-relaxed text-lg mb-4">
                     <strong>Technology what I use</strong>
                     <ul className="list-disc list-inside">
-                      <li>HTML</li>
+                      <li>React.js</li>
                     </ul>
                     <ul className="list-disc list-inside">
                       <li>SASS</li>
-                    </ul>
-                    <ul className="list-disc list-inside">
-                      <li>JavaScript</li>
                     </ul>
                   </p>
                   <a href="https://github.com/nuflakbrr/react-hangman" target="_blank" rel="noreferrer" className="text-blue-600 inline-flex items-center hover:text-blue-400">
