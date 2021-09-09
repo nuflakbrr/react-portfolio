792b47b6327358db3fa4bfe497b5c6f3949b043f
Naufal Akbar <kbrnugroho@gmail.com>
2021-09-09 11:00:35 +0700

    change alt img about

--- src/pages/about/About.js
+++ src/pages/about/About.js
@@ -13,7 +13,7 @@ function About() {
         <section className="text-gray-400 bg-gray-900 body-font">
           <div className="container mx-auto flex flex-col px-5 py-24 justify-center items-center">
             <Fade>
-              <img className="lg:w-1/5 md:w-3/6 w-5/6 mb-10 object-cover object-center rounded" alt="Profile" src={Profile} />
+              <img className="lg:w-1/5 md:w-3/6 w-5/6 mb-10 object-cover object-center rounded" alt="Profile Image" src={Profile} />
             </Fade>
 
             <Fade bottom cascade>
